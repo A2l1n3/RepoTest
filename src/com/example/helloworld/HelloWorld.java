bda2c154d2062acc6c4f21cadb9a80b4993153d4
A2l1n3 <chlorys79@yahoo.fr>
2017-02-05 15:48:59 +0100

    ajout d'une classe

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -4,8 +4,12 @@ package com.example.helloworld;
  * Created by adeline on 05/02/17.
  */
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello Git");
+    public String Hello() {
+        return Hello();
+    }
+
+    public String Hello(String value){
+        return "Hello " + value;
     }
 }
 
---
+++ src/com/example/test/test.java
@@ -0,0 +1,18 @@
+package com.example.test;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import static com.example.helloworld.HelloWorld.*;
+
+/**
+ * Created by adeline on 05/02/17.
+ */
+public class test {
+    public static void main(String args){
+        System.out.println("quel est ton nom ? " );
+      //  BufferedReader nom = new BufferedReader(new InputStreamReader(System.in));
+       // System.out.println(Hello(nom.toString()));
+}
+}
